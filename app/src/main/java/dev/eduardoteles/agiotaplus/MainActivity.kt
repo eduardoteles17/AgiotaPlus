3327562a33323f38ef254f73512cc86064a69abf
eduardoteles17 <edu.teles@pm.me>
2023-06-09 16:40:51 -0300

    chore: init project

---
+++ app/src/main/java/dev/eduardoteles/agiotaplus/MainActivity.kt
@@ -0,0 +1,40 @@
+package dev.eduardoteles.agiotaplus
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.eduardoteles.agiotaplus.ui.theme.AgiotaPlusTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AgiotaPlusTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AgiotaPlusTheme {
+        Greeting("Android")
+    }
+}
